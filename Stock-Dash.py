5503ca5e989cf912b5b69a240ffbb9a74ee8b5e3
Ks-Nath <86104360+Ks-Nath@users.noreply.github.com>
2024-09-05 19:47:30 +0530

    Update Stock-Dash.py

--- Stock-Dash.py
+++ Stock-Dash.py
@@ -36,16 +36,16 @@ with fund_data:
         bs.columns = list(balance_sheet.T.iloc[0])
         st.write(bs)
 
-        st.subheader('Income Statement')
-        income_statement = fd.get_income_statement_annual(ticker)[0]
-        is1 = income_statement.T[2:]
-        is1.columns = list(income_statement.T.iloc[0])
-        st.write(is1)
-        
         st.subheader('Cash Flow Statement')
         cash_flow = fd.get_cash_flow_annual(ticker)[0]
         cf = cashflow.T[2:]
         cf.columns = list(cash_flow.T.iloc[0])
+        st.write(cf)
+        
+        st.subheader('Income Statement')
+        income_statement = fd.get_income_statement_annual(ticker)[0]
+        is1 = income_statement.T[2:]
+        is1.columns = list(income_statement.T.iloc[0])
         st.write(is1)
       
     except:
